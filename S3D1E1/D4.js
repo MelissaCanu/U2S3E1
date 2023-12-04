fd81dd5575ae8ad712f33fd3a4fae27fb067df52
MelissaCanu <melissamcanu@gmail.com>
2023-12-04 15:16:33 +0100

    ci ho ragionato ma ce l'ho fatta ex.5

--- S3D1E1/D4.js
+++ S3D1E1/D4.js
@@ -79,6 +79,17 @@ console.log(boundary(99));
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let epify = function (string) {
+  if (string.startsWith("EPICODE")) {
+    return string;
+  } else {
+    return "EPICODE " + string;
+  }
+};
+
+console.log(epify("EPICODE is hard, innit"));
+console.log(epify("doesn't start with that word"));
+
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
